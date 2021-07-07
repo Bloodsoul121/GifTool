38b8998faa99f177442b36057590bbece64cd4b1
caiguangzu.cgz <caiguangzu.cgz@alibaba-inc.com>
2021-07-07 12:14:27 +0800

    support 87a 89a

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -11,6 +11,9 @@ extern "C" {
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
 #define argb(a, r, g, b) (((a) & 0xff) << 24 | ((b) & 0xff) << 16 | ((g) & 0xff) << 8 | ((r) & 0xff))
+#define dispose(ext) (((ext)->Bytes[0] & 0x1c) >> 2)
+#define trans_index(ext) ((ext)->Bytes[3])
+#define transparency(ext) ((ext)->Bytes[0] & 1)
 
 struct GifBean {
     int next_frame;
@@ -19,6 +22,8 @@ struct GifBean {
 };
 
 int drawFrame(GifFileType *gifFileType, AndroidBitmapInfo bitmapInfo, void *pixels);
+
+int drawFrame(GifFileType *gif, AndroidBitmapInfo info, void *pixels, bool force_dispose_1);
 
 extern "C"
 JNIEXPORT jlong JNICALL
@@ -78,7 +83,7 @@ Java_com_blood_giftool_GifHelper_nativeUpdateFrame(JNIEnv *env, jclass clazz, jl
     AndroidBitmap_lockPixels(env, bitmap, &pixels);
 
     // 绘制
-    int delay = drawFrame(gifFileType, androidBitmapInfo, pixels);
+    int delay = drawFrame(gifFileType, androidBitmapInfo, pixels, false);
 
     AndroidBitmap_unlockPixels(env, bitmap);
 
@@ -126,6 +131,118 @@ int drawFrame(GifFileType *gifFileType, AndroidBitmapInfo bitmapInfo, void *pixe
     }
 
     return 33;
+}
+
+// 兼容 89a 87a 图片
+// 89a 取自每一帧 SavedImage 里面的颜色字典
+// 87a 取自外部 GifFileType 统一的颜色字典
+int drawFrame(GifFileType *gif, AndroidBitmapInfo info, void *pixels, bool force_dispose_1) {
+    GifColorType *bg;
+    GifColorType *color;
+    SavedImage *frame;
+    ExtensionBlock *ext = 0;
+    GifImageDesc *frameInfo;
+    ColorMapObject *colorMap;
+    int *line;
+    int width, height, x, y, j, loc, n, inc, p;
+    void *px;
+    GifBean *gifBean = static_cast<GifBean *>(gif->UserData);
+    width = gif->SWidth;
+    height = gif->SHeight;
+    frame = &(gif->SavedImages[gifBean->next_frame]);
+    frameInfo = &(frame->ImageDesc);
+    if (frameInfo->ColorMap) {
+        colorMap = frameInfo->ColorMap;
+    } else {
+        colorMap = gif->SColorMap;
+    }
+
+    bg = &colorMap->Colors[gif->SBackGroundColor];
+
+    for (j = 0; j < frame->ExtensionBlockCount; j++) {
+        if (frame->ExtensionBlocks[j].Function == GRAPHICS_EXT_FUNC_CODE) {
+            ext = &(frame->ExtensionBlocks[j]);
+            break;
+        }
+    }
+    // For dispose = 1, we assume its been drawn
+    px = pixels;
+    if (ext && dispose(ext) == 1 && force_dispose_1 && gifBean->next_frame > 0) {
+        gifBean->next_frame = gifBean->next_frame - 1,
+                drawFrame(gif, info, pixels, true);
+    } else if (ext && dispose(ext) == 2 && bg) {
+        for (y = 0; y < height; y++) {
+            line = (int *) px;
+            for (x = 0; x < width; x++) {
+                line[x] = argb(255, bg->Red, bg->Green, bg->Blue);
+            }
+            px = (int *) ((char *) px + info.stride);
+        }
+    } else if (ext && dispose(ext) == 3 && gifBean->next_frame > 1) {
+        gifBean->next_frame = gifBean->next_frame - 2,
+                drawFrame(gif, info, pixels, true);
+    }
+    px = pixels;
+    if (frameInfo->Interlace) {
+        n = 0;
+        inc = 8;
+        p = 0;
+        px = (int *) ((char *) px + info.stride * frameInfo->Top);
+        for (y = frameInfo->Top; y < frameInfo->Top + frameInfo->Height; y++) {
+            for (x = frameInfo->Left; x < frameInfo->Left + frameInfo->Width; x++) {
+                loc = (y - frameInfo->Top) * frameInfo->Width + (x - frameInfo->Left);
+                if (ext && frame->RasterBits[loc] == trans_index(ext) && transparency(ext)) {
+                    continue;
+                }
+                color = (ext && frame->RasterBits[loc] == trans_index(ext)) ? bg
+                                                                            : &colorMap->Colors[frame->RasterBits[loc]];
+                if (color)
+                    line[x] = argb(255, color->Red, color->Green, color->Blue);
+            }
+            px = (int *) ((char *) px + info.stride * inc);
+            n += inc;
+            if (n >= frameInfo->Height) {
+                n = 0;
+                switch (p) {
+                    case 0:
+                        px = (int *) ((char *) pixels + info.stride * (4 + frameInfo->Top));
+                        inc = 8;
+                        p++;
+                        break;
+                    case 1:
+                        px = (int *) ((char *) pixels + info.stride * (2 + frameInfo->Top));
+                        inc = 4;
+                        p++;
+                        break;
+                    case 2:
+                        px = (int *) ((char *) pixels + info.stride * (1 + frameInfo->Top));
+                        inc = 2;
+                        p++;
+                }
+            }
+        }
+    } else {
+        px = (int *) ((char *) px + info.stride * frameInfo->Top);
+        for (y = frameInfo->Top; y < frameInfo->Top + frameInfo->Height; y++) {
+            line = (int *) px;
+            for (x = frameInfo->Left; x < frameInfo->Left + frameInfo->Width; x++) {
+                loc = (y - frameInfo->Top) * frameInfo->Width + (x - frameInfo->Left);
+                if (ext && frame->RasterBits[loc] == trans_index(ext) && transparency(ext)) {
+                    continue;
+                }
+                color = (ext && frame->RasterBits[loc] == trans_index(ext)) ? bg
+                                                                            : &colorMap->Colors[frame->RasterBits[loc]];
+                if (color)
+                    line[x] = argb(255, color->Red, color->Green, color->Blue);
+            }
+            px = (int *) ((char *) px + info.stride);
+        }
+    }
+    GraphicsControlBlock gcb;//获取控制信息
+    DGifSavedExtensionToGCB(gif, gifBean->next_frame, &gcb);
+    int delay = gcb.DelayTime * 10; // gcb.DelayTime 单位10ms，转为ms，就要 *10
+    LOGD("delay %d", delay);
+    return delay; // ms
 }
 
 extern "C"
--- app/src/main/java/com/blood/giftool/MainActivity.java
+++ app/src/main/java/com/blood/giftool/MainActivity.java
@@ -25,16 +25,29 @@ public class MainActivity extends AppCompatActivity {
     private final Handler mHandler = new Handler(Looper.getMainLooper()) {
         @Override
         public void handleMessage(@NonNull Message msg) {
-            int delay = mGifHelper.updateFrame(mBitmap);
-            binding.iv.setImageBitmap(mBitmap);
-            mHandler.sendEmptyMessageDelayed(0, delay);
+            switch (msg.what) {
+                case 0:
+                    int delay1 = mGifHelper87a.updateFrame(mBitmap87a);
+                    binding.iv87a.setImageBitmap(mBitmap87a);
+                    mHandler.sendEmptyMessageDelayed(0, delay1);
+                    break;
+                case 1:
+                    int delay2 = mGifHelper89a.updateFrame(mBitmap89a);
+                    binding.iv89a.setImageBitmap(mBitmap89a);
+                    mHandler.sendEmptyMessageDelayed(1, delay2);
+                    break;
+            }
+
         }
     };
 
     private ActivityMainBinding binding;
-    private String gifPath;
-    private GifHelper mGifHelper;
-    private Bitmap mBitmap;
+    private String mGifPath87a;
+    private String mGifPath89a;
+    private GifHelper mGifHelper87a;
+    private GifHelper mGifHelper89a;
+    private Bitmap mBitmap87a;
+    private Bitmap mBitmap89a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,30 +61,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mHandler.removeCallbacksAndMessages(null);
-        if (mGifHelper != null) {
-            mGifHelper.release();
+        if (mGifHelper87a != null) {
+            mGifHelper87a.release();
+        }
+        if (mGifHelper89a != null) {
+            mGifHelper89a.release();
         }
     }
 
-    private String copyAssetsGif() {
-        String gifFileName = "gif_89a.gif";
+    private String copyAssetsGif(String gifFileName) {
         File dstFile = new File(getFilesDir(), gifFileName);
         FileUtil.copyAssets(this, gifFileName, dstFile);
         return dstFile.getAbsolutePath();
     }
 
     private void init() {
-        gifPath = copyAssetsGif();
-        binding.nativeLoad.setOnClickListener(v -> nativeLoadGif());
+        mGifPath87a = copyAssetsGif("gif_87a.gif");
+        mGifPath89a = copyAssetsGif("gif_89a.gif");
+        binding.nativeLoad87a.setOnClickListener(v -> nativeLoadGif87a());
+        binding.nativeLoad89a.setOnClickListener(v -> nativeLoadGif89a());
     }
 
-    private void nativeLoadGif() {
-        mGifHelper = GifHelper.loadGifPath(gifPath);
-        int width = mGifHelper.getWidth();
-        int height = mGifHelper.getHeight();
-        Log.i(TAG, "nativeLoadGif: width " + width + " , height " + height);
-        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+    private void nativeLoadGif87a() {
+        mGifHelper87a = GifHelper.loadGifPath(mGifPath87a);
+        int width = mGifHelper87a.getWidth();
+        int height = mGifHelper87a.getHeight();
+        Log.i(TAG, "nativeLoadGif87a: width " + width + " , height " + height);
+        mBitmap87a = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mHandler.removeMessages(0);
         mHandler.sendEmptyMessage(0);
+    }
+
+    private void nativeLoadGif89a() {
+        mGifHelper89a = GifHelper.loadGifPath(mGifPath89a);
+        int width = mGifHelper89a.getWidth();
+        int height = mGifHelper89a.getHeight();
+        Log.i(TAG, "nativeLoadGif89a: width " + width + " , height " + height);
+        mBitmap89a = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mHandler.removeMessages(1);
+        mHandler.sendEmptyMessage(1);
     }
 
 }
